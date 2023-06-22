4d5ef40e54eb24ae92af8589ac82055513995fac
Marlowe Recht <recht.marlowe@gmail.com>
2023-06-22 16:03:25 -0400

    created contactRoutes file and created get route

--- my-contacts-backend/server.js
+++ my-contacts-backend/server.js
@@ -5,9 +5,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get("/api/contacts", (req, res) => {
-    res.status(200).json({ message: "Get all contacts" });
-});
+//middleware
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
     console.log(`hi from ${port}`)
