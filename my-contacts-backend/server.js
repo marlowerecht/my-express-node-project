34230e682d0ccd7e9e622b143b411b672c5ebe04
Marlowe Recht <recht.marlowe@gmail.com>
2023-06-26 14:18:38 -0400

    created error handler

--- my-contacts-backend/server.js
+++ my-contacts-backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
@@ -8,6 +9,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`hi from ${port}`)
