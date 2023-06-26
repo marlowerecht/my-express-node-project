c2008a215d6513f1359549a24d01b1f5abd35d45
Marlowe Recht <recht.marlowe@gmail.com>
2023-06-26 15:27:46 -0400

    mongoDB setup

--- my-contacts-backend/controllers/contactController.js
+++ my-contacts-backend/controllers/contactController.js
@@ -10,7 +10,7 @@ const getContact = asyncHandler(async (req, res) => {
 //@description Create new contact
 //@route POST /api/contacts
 //@access public
-const createContact = async (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     console.log(req.body)
     const { name, email, phone } = req.body
     if (!name || !email || !phone) {
@@ -18,27 +18,27 @@ const createContact = async (req, res) => {
         throw new Error ("All fields are mandatory")
     }
     res.status(201).json({ message: "Create Contact" });
-}; 
+}); 
 
 //@description Get one contact
 //@route GET /api/contacts/:id
 //@access public
-const getOneContact = async (req, res) => {
+const getOneContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Get one contact"})
-}
+});
 
 //@description Update contact
 //@route PUT /api/contacts/:id
 //@access public
-const updateContact = async (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Update contact"})
-}
+});
 
 //@description Delete contact
 //@route DELETE /api/contacts/:id
 //@access public
-const deleteContact = async (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Delete contact"})
-}
+});
 
 module.exports = { getContact, createContact, getOneContact, updateContact, deleteContact };
